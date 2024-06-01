d5dfa221bfc377ffc5aa5f354ffe79a36d248179
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2024-06-01 09:08:04 -0300

    feat: add buba clone event

--- src/discord/events/events.controller.ts
+++ src/discord/events/events.controller.ts
@@ -7,6 +7,7 @@ import {
 } from 'discord.js';
 import { DiscordArtsApproveService } from './service/arts-approve.service';
 import { DiscordAddNickToWhitelistService } from './service/add-nicks-to-whitelist.service';
+import { DiscordBubaCloneService } from './service/buba-clone.service';
 export class DiscordEventsController {
   artAprroveChannelId = process.env.APPROVE_ARTS_CHANNEL_ID;
   nicksWhitelistChannelId = process.env.NICKS_CHANNEL_ID;
@@ -21,7 +22,14 @@ export class DiscordEventsController {
 
   async messageCreate(message: Message) {
     if (message.author.bot) return;
-    
+
+    if (
+      message.author.id === process.env.BUBA_ID &&
+      process.env.NODE_ENV === 'develop'
+    ) {
+      return new DiscordBubaCloneService().handle(message);
+    }
+
     if (message.channel.id === this.nicksWhitelistChannelId) {
       return await new DiscordAddNickToWhitelistService().handle(message);
     }
---
+++ src/discord/events/service/buba-clone.service.ts
@@ -0,0 +1,26 @@
+import { Message } from 'discord.js';
+
+export class DiscordBubaCloneService {
+  activeLoop = true;
+  async handle(message: Message) {
+    try {
+      if (message.content) {
+        if (message.content === 'para') {
+          this.activeLoop = false;
+        }
+        if (message.content === 'volta') {
+          this.activeLoop = true;
+        }
+        if (!this.activeLoop) return;
+        message.channel.send(message.content);
+      }
+      if (message.attachments) {
+        message.attachments.forEach((attachment) => {
+          message.channel.send(attachment.url);
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
